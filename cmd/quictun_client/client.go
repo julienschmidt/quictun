0b9fce837621560c2d1d4e9c8b9cfddcb56af065
Julien Schmidt <julien.schmidt@tum.de>
2018-01-03 20:12:52 +0100

    set example client/server config via command line flags

--- cmd/quictun_client/client.go
+++ cmd/quictun_client/client.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/julienschmidt/quictun"
 )
 
-const listenAddr = "localhost:1080"
-const tunnelAddr = "https://very:sicher@quic.clemente.io:6121/secret"
+const (
+	// the User-Agent string is not observable, but should have the same length as a regular browser UA, e.g. that of Chrome
+	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 X-quictun/0.1"
 
-const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-
-const dialTimeout = 30
+	// timeout for establishing connections to quictun server (in seconds)
+	dialTimeout = 30
+)
 
 func main() {
+	// command-line flags and args
+	listenFlag := flag.String("l", "localhost:1080", "local SOCKS listen address")
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [OPTIONS] QUICTUN_URL\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		flag.Usage()
+		return
+	}
+	tunnelAddr := args[0]
+
+	// configure and run quictun client
 	client := quictun.Client{
-		ListenAddr:  listenAddr,
+		ListenAddr:  *listenFlag,
 		TunnelAddr:  tunnelAddr,
 		UserAgent:   userAgent,
 		DialTimeout: dialTimeout * time.Second,
